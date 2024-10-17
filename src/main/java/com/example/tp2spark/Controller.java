56017f503ed5a50ca8856c5ef9fe118d7be5047d
Abel Quispe <quisperobledoabel@gmail.com>
2024-10-17 19:01:53 -0300

    conection

--- src/main/java/com/example/tp2spark/Controller.java
+++ src/main/java/com/example/tp2spark/Controller.java
@@ -1,7 +1,5 @@
 package com.example.tp2spark;
 
-import org.sql2o.Connection;
-
 import com.google.gson.Gson;
 
 import spark.Route;
@@ -9,28 +7,20 @@ import spark.Request;
 import spark.Response;
 
 public class Controller {
-  private static DAO servicio;
+  private static DAOPropiedad servicio;
   private final static Gson gson = new Gson();
 
-  static {
-    try (Connection con = DbConexion.getSql2o().open()) {
-        servicio = new DAO(con);
-    } catch (Exception e) {
-        System.err.println("Error initializing DAO: " + e.getMessage());
-    }
-}
-
   public static Route getPropiedades = (Request req, Response res) -> {
-      try {
-        res.status(200);
-        return gson.toJson(servicio.getAllPropiedades());
-      } catch (Exception e) {
-        res.status(400);
-        return new Gson().toJson("Error controlador: " + e.getMessage());
-      }
+    try {
+      res.status(200);
+      return gson.toJson(servicio.getAllPropiedades());
+    } catch (Exception e) {
+      res.status(400);
+      return new Gson().toJson("Error controlador: " + e.getMessage());
+    }
   };
 
-  public static Route getPropiedad = (Request req, Response res) ->{
+  public static Route getPropiedad = (Request req, Response res) -> {
     try {
       res.status(200);
       String id = req.params(":id");
@@ -41,7 +31,7 @@ public class Controller {
     }
   };
 
-  public static Route addPropiedad = (Request req, Response res) ->{
+  public static Route addPropiedad = (Request req, Response res) -> {
     Integer id = (int) (Math.random() * 100000);
     String ubicacion = req.queryParams("ubicacion");
     String tipoPropiedad = req.queryParams("tipo").toUpperCase();
@@ -49,8 +39,9 @@ public class Controller {
     Integer ambientes = Integer.valueOf(req.queryParams("ambientes"));
     Integer banios = Integer.valueOf(req.queryParams("banios"));
     Integer mts_cuadrados = Integer.valueOf(req.queryParams("mtsCuadrados"));
-    
-    Propiedad propiedad = new Propiedad(id, ubicacion, tipoPropiedad, destinoPropiedad, ambientes, banios, mts_cuadrados);
+
+    Propiedad propiedad = new Propiedad(id, ubicacion, tipoPropiedad, destinoPropiedad, ambientes, banios,
+        mts_cuadrados);
 
     try {
       res.status(200);
@@ -61,7 +52,7 @@ public class Controller {
     }
   };
 
-  public static Route updatePropiedad = (Request req, Response res) ->{
+  public static Route updatePropiedad = (Request req, Response res) -> {
     Integer id = Integer.valueOf(req.queryParams("id"));
     String ubicacion = req.queryParams("ubicacion");
     String tipoPropiedad = req.queryParams("tipo").toUpperCase();
@@ -69,8 +60,9 @@ public class Controller {
     Integer ambientes = Integer.valueOf(req.queryParams("ambientes"));
     Integer banios = Integer.valueOf(req.queryParams("banios"));
     Integer mts_cuadrados = Integer.valueOf(req.queryParams("mtsCuadrados"));
-    
-    Propiedad propiedad = new Propiedad(id, ubicacion, tipoPropiedad, destinoPropiedad, ambientes, banios, mts_cuadrados);
+
+    Propiedad propiedad = new Propiedad(id, ubicacion, tipoPropiedad, destinoPropiedad, ambientes, banios,
+        mts_cuadrados);
     try {
       res.status(200);
       return gson.toJson(servicio.updatePropiedad(propiedad));
@@ -78,7 +70,7 @@ public class Controller {
       res.status(400);
       return new Gson().toJson("Error controlador: " + e.getMessage());
     }
-    
+
   };
 
   public static Route deletePropiedad = (Request req, Response res) -> {
--- src/main/java/com/example/tp2spark/DAOPropiedad.java
+++ src/main/java/com/example/tp2spark/DAOPropiedad.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 import org.sql2o.Connection;
 
-public class DAO implements DAOInterface {
+public class DAOPropiedad implements DAOInterface {
   private final Connection con;
 
-  public DAO(Connection con) {
-    this.con = con;
+  public DAOPropiedad() {
+    this.con = DbConexion.getSql2o().open();
   }
-  
+
   @Override
   public List<Propiedad> getAllPropiedades() {
     List<Propiedad> propiedades;
@@ -25,21 +25,24 @@ public class DAO implements DAOInterface {
 
   @Override
   public Propiedad getPropiedadById(String id) {
-    
+
     try {
       return con.createQuery("SELECT * FROM PROPIEDADES WHERE ID = " + id + ";").executeAndFetchFirst(Propiedad.class);
     } catch (Exception e) {
       System.err.println("Error al ejecutar la query: " + e.getMessage());
       return null;
     }
-    
+
   }
 
   @Override
   public Propiedad updatePropiedad(Propiedad propiedad) {
     try {
-      con.createQuery("UPDATE PROPIEDADES SET ubicacion='" + propiedad.getUbicacion() + "', TIPO='" + propiedad.getTipo() + "', DESTINO='" + propiedad.getDestino() + "', AMBIENTES=" + propiedad.getAmbientes().toString() + ", BANIOS=" + propiedad.getBanios().toString() + ", MTS_CUADRADOS=" + propiedad.getMts_cuadrados().toString() + " WHERE ID = " + propiedad.getId() + ";").executeUpdate();
-    
+      con.createQuery("UPDATE PROPIEDADES SET ubicacion='" + propiedad.getUbicacion() + "', TIPO='"
+          + propiedad.getTipo() + "', DESTINO='" + propiedad.getDestino() + "', AMBIENTES="
+          + propiedad.getAmbientes().toString() + ", BANIOS=" + propiedad.getBanios().toString() + ", MTS_CUADRADOS="
+          + propiedad.getMts_cuadrados().toString() + " WHERE ID = " + propiedad.getId() + ";").executeUpdate();
+
     } catch (Exception e) {
       System.err.println("Error al ejecutar la query: " + e.getMessage());
       return null;
@@ -50,7 +53,12 @@ public class DAO implements DAOInterface {
   @Override
   public boolean addPropiedad(Propiedad propiedad) {
     try {
-      con.createQuery("INSERT INTO propiedades (ID, UBICACION, TIPO, DESTINO, AMBIENTES, BANIOS, MTS_CUADRADOS) VALUES (" + propiedad.getId() + ", '" + propiedad.getUbicacion() + "', '" + propiedad.getTipo() + "', '" + propiedad.getDestino() + "', " + propiedad.getAmbientes() + ", " + propiedad.getBanios() + ", " + propiedad.getMts_cuadrados() + ");").executeUpdate();
+      con.createQuery(
+          "INSERT INTO propiedades (ID, UBICACION, TIPO, DESTINO, AMBIENTES, BANIOS, MTS_CUADRADOS) VALUES ("
+              + propiedad.getId() + ", '" + propiedad.getUbicacion() + "', '" + propiedad.getTipo() + "', '"
+              + propiedad.getDestino() + "', " + propiedad.getAmbientes() + ", " + propiedad.getBanios() + ", "
+              + propiedad.getMts_cuadrados() + ");")
+          .executeUpdate();
       return true;
     } catch (Exception e) {
       System.err.println("Error al ejecutar la query: " + e.getMessage());
@@ -61,7 +69,7 @@ public class DAO implements DAOInterface {
   @Override
   public String deletePropiedad(String id) {
     try {
-      con.createQuery("DELETE FROM propiedades WHERE id = " + id + ";" ).executeUpdate();
+      con.createQuery("DELETE FROM propiedades WHERE id = " + id + ";").executeUpdate();
       return id;
     } catch (Exception e) {
       System.err.println("Error al ejecutar la query: " + e.getMessage());
